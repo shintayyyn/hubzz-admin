07a5e62118ed8fa803a2c3649824ff7980ab7dfc
z4rl <zpvedad.halcyondigital@gmail.com>
2019-07-23 19:48:23 +0800

    build: merge with mariel

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,4 +1,1 @@
-importScripts(
-  '/sw.js?1563795888172',
-  'https://cdn.onesignal.com/sdks/OneSignalSDK.js'
-)
+importScripts('/sw.js?1563868689430', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
