6b28c59e19bce87d9a6def39c39708cc6eb27bf5
Mariel <mbmendoza.halcyondigital@gmail.com>
2019-10-03 08:51:10 +0800

    ci: minor changes

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-importScripts('/sw.js?1564698938721', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
-=======
-importScripts('/sw.js?1569983673116', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
->>>>>>> fca06d4d31f77c8dcc9fa5c8bc42c165152fd391
+importScripts('/sw.js?1570063315171', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
