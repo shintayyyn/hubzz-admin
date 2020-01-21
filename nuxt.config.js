90799a99bb37b7fddcf87dc41cdb8173ce3354d0
Arvi <cacheng.halcyondigital@gmail.com>
2020-01-21 08:20:00 +0800

    ci: asd

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,15 @@
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
-import path from "path";
-
-import pkg from "./package";
+import path from "path"
 
 export default {
 	mode: "universal",
 
 	env: {
 		API_URL: process.env.API_URL,
-		ONE_SIGNAL_APP_ID_ADMIN: process.env.ONE_SIGNAL_APP_ID_ADMIN
+		ONE_SIGNAL_APP_ID_ADMIN: process.env.ONE_SIGNAL_APP_ID_ADMIN,
 	},
 
 	/*
@@ -22,108 +20,89 @@ export default {
 		meta: [
 			{ charset: "utf-8" },
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
-			{ hid: "description", name: "description", content: pkg.description }
+			{ hid: "description", name: "description", content: 'Hubzz Web App' },
 		],
 		link: [
 			{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
 			{
 				rel: "stylesheet",
 				href: "https://fonts.googleapis.com/css?family=Nunito"
-			}
+			},
 		],
 		script: [
 			{
-				src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"
+				src: "https://cdn.onesignal.com/sdks/OneSignalSDK.js",
+				defer: true,
 			},
 			{
-				src:
-					"https://maps.googleapis.com/maps/api/js?key=AIzaSyCqkvPPMK1lBnOF1NpxUggGFzkfcoaHV24&libraries=places"
-			}
+				src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js",
+			},
+			{
+				src: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCqkvPPMK1lBnOF1NpxUggGFzkfcoaHV24&libraries=places",
+			},
 		]
 	},
 
-	/*
-	 ** Customize the progress-bar color
-	 */
 	loading: { color: "#FFC72C" },
 
-	/*
-	 ** Global CSS
-	 */
 	css: [
 		"~/assets/css/tailwind.css",
 		"~/assets/css/main.css",
 		"quill/dist/quill.snow.css",
 		"quill/dist/quill.bubble.css",
-		"quill/dist/quill.core.css"
+		"quill/dist/quill.core.css",
 	],
 
-	/*
-	 ** Plugins to load before mounting the App
-	 */
 	plugins: [
 		{
 			src: "@/plugins/socket-io.js",
-			ssr: false
+			mode: "client",
 		},
 		{
 			src: "@/plugins/one-signal.js",
-			ssr: false
+			mode: "client",
 		},
 		{
 			src: "@/plugins/vue-svgicon.js",
-			ssr: true
+			ssr: true,
 		},
 		{
 			src: "@/plugins/moment.js",
-			ssr: true
+			ssr: true,
 		},
 		{
 			src: "~/plugins/google-maps",
-			ssr: true
+			ssr: true,
 		},
 		{
 			src: "~plugins/nuxt-quill-plugin.js",
-			ssr: false
+			ssr: false,
 		},
 		{
 			src: "@/plugins/mixin.js",
-			ssr: true
+			ssr: true,
 		},
 		{
 			src: "@/plugins/filters.js",
-			ssr: true
+			ssr: true,
 		},
 		{
 			src: "@/plugins/jspdf.js",
-			mode: "client"
+			mode: "client",
 		},
 		{
 			src: "@/plugins/html2canvas.js",
-			mode: "client"
-		}
+			mode: "client",
+		},
 	],
 
-	/*
-	 ** Nuxt.js modules
-	 */
 	modules: [
-		// Doc: https://axios.nuxtjs.org/usage
 		"@nuxtjs/axios",
 		"@nuxtjs/auth",
-		// ['nuxt-google-maps-module', {
-		//   /* module options */
-		//   key: 'GOOGLE MAPS KEY',
-		// }],
-		"@nuxtjs/onesignal",
-		"@nuxtjs/pwa",
 	],
-	/*
-	 ** Axios module configuration
-	 */
+
 	axios: {
-		// See https://github.com/nuxt-community/axios-module#options
-		baseURL: process.env.API_URL
+		baseURL: process.env.API_URL,
 	},
 
 	auth: {
@@ -135,7 +114,7 @@ export default {
 			options: {
 				path: "/",
 				expires: 1825
-			}
+			},
 		},
 		strategies: {
 			local: {
@@ -145,44 +124,27 @@ export default {
 						url: "/api/v1/admin/me",
 						method: "get",
 						propertyName: "data.user"
-					}
-				}
-			}
-		}
-	},
-
-	oneSignal: {
-		init: {
-			appId: process.env.ONE_SIGNAL_APP_ID_ADMIN,
-			allowLocalhostAsSecureOrigin: true,
-			welcomeNotification: {
-				disable: true
-			}
-		}
+					},
+				},
+			},
+		},
 	},
 
 	vue: {
 		config: {
 			productionTip: false,
-			devtools: true
-		}
+			devtools: true,
+		},
 	},
 
-	/*
-	 ** Build configuration
-	 */
 	build: {
 		postcss: {
 			plugins: {
 				tailwindcss: path.join(__dirname, 'tailwind.config.js'),
 				autoprefixer: {},
-			}
+			},
 		},
-
-		/*
-		 ** You can extend webpack config here
-		 */
 		transpile: [/^vue2-google-maps($|\/)/],
-		extend(config, ctx) {}
-	}
-};
+		extend(config, ctx) {},
+	},
+}
