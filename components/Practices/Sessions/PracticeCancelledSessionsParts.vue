f20af170f1a0d9ef9e06e16b546d8152cf00880e
Arvi <cacheng.halcyondigital@gmail.com>
2020-02-27 13:49:23 +0800

    ...

--- components/Practices/Sessions/PracticeCancelledSessionsParts.vue
+++ components/Practices/Sessions/PracticeCancelledSessionsParts.vue
@@ -1,74 +1,75 @@
 <template>
-    <div>
-      <div class="overflow-x-auto overflow-y-hidden">
-        <div v-if="cancelledJobs.length === 0">
-          <div
-          class="mt-10 w-full text-center text-white"
-          >This practice had no case of cancelled session/s.</div>
-        </div>
-        <div v-else>
-          <AppJobHeaderSort :practice="practice" :tabStatus="'Cancelled'" :currentPage="currentPage" :isJobParts="false"/>
-            <div class="w-full overflow-x-auto" > 
-            <!-- BODY -->
-            <nuxt-link 
-              v-for="(item, index) in cancelledJobs" 
-              :to="checkRoute(item.id)"
-              :key="`item-${index}`" 
-                class="flex flex-col cursor-pointer md:flex-row px-4 md:px-0 py-2 my-2 rounded-lg border-l-8 border-yellow-500 md:border-l-0 text-white no-underline shadow-lg bg-waterloo hover:bg-waterloo-light" 
-              draggable="false"
-            >
-              <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle">
-                <strong class="block md:hidden text-sm uppercase">Job Number</strong>
-                <span class="break-words">{{item.job_part_number}}</span>
-              </div>
-              <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle md:text-center">
-                <strong class="block md:hidden text-sm uppercase">Practice / Surgery</strong>
-                <span class="break-words">{{item.job.platform_job.practice.surgery.name}}</span>
-              </div>
-              <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle md:text-center">
-                <strong class="block md:hidden text-sm uppercase">Title</strong>
-                <span class="break-words">{{item.job.title}}</span>
-              </div>
-              <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle md:text-center">
-                <strong class="block md:hidden text-sm uppercase">From</strong>
-                <span class="break-words">{{$moment(item.date_start,'YYYY-MM-DD[T]').format('DD/MM/YYYY')}}</span>
-              </div>
-              <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle md:text-center">
-                <strong class="block md:hidden text-sm uppercase">To</strong>
-                <span class="break-words">{{$moment(item.date_end,'YYYY-MM-DD[T]').format('DD/MM/YYYY')}}</span>
-              </div>
-              <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle md:text-center">
-                <strong class="block md:hidden text-sm uppercase">Created</strong>
-                <span class="">{{ $moment(item.created_at, 'YYYY-MM-DD[T]HH:mm:ss.SSS[Z]').format('DD/MM/YYYY, h:mm:ss a') }}</span>
-              </div>
-            </nuxt-link>
-          </div>
-        </div>
-        <div v-if="!cancelledJobs.length == 0" class="">
-          <AppPagination
-            :total="total"
-            :totalPages="totalPages"
-            :currentPage="currentPage"
-            :perPage="perPage"
-            @pagechanged="pagechanged"
-          />
-        </div> 
-
-        <div class="job-shield" v-if="modal"></div>
-        <transition name="slide" mode="out-in">
-          <div class="job-modal shadow-lg" v-if="modal">
-            <PracticeSessionModal @close="modal = false" :job="job" />
-          </div>
-        </transition>
-
+  <div>
+    <div class="overflow-x-auto overflow-y-hidden">
+      <div v-if="cancelledJobs.length === 0">
+        <div
+        class="mt-10 w-full text-center text-white"
+        >This practice had no case of cancelled session/s.</div>
       </div>
+      <div v-else>
+        <AppJobHeaderSort :practice="practice" :tabStatus="'Cancelled'" :currentPage="currentPage" :isJobParts="false"/>
+          <div class="w-full overflow-x-auto" > 
+          <!-- BODY -->
+          <nuxt-link 
+            v-for="(item, index) in cancelledJobs" 
+            :to="checkRoute(item.id)"
+            :key="`item-${index}`" 
+              class="flex flex-col cursor-pointer md:flex-row px-4 md:px-0 py-2 my-2 rounded-lg border-l-8 border-yellow-500 md:border-l-0 text-white no-underline shadow-lg bg-waterloo hover:bg-waterloo-light" 
+            draggable="false"
+          >
+            <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle">
+              <strong class="block md:hidden text-sm uppercase">Job Number</strong>
+              <span class="break-words">{{item.job_part_number}}</span>
+            </div>
+            <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle md:text-center">
+              <strong class="block md:hidden text-sm uppercase">Practice / Surgery</strong>
+              <span class="break-words">{{item.job.platform_job.practice.surgery.name}}</span>
+            </div>
+            <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle md:text-center">
+              <strong class="block md:hidden text-sm uppercase">Title</strong>
+              <span class="break-words">{{item.job.title}}</span>
+            </div>
+            <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle md:text-center">
+              <strong class="block md:hidden text-sm uppercase">From</strong>
+              <span class="break-words">{{$moment(item.date_start,'YYYY-MM-DD[T]').format('DD/MM/YYYY')}}</span>
+            </div>
+            <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle md:text-center">
+              <strong class="block md:hidden text-sm uppercase">To</strong>
+              <span class="break-words">{{$moment(item.date_end,'YYYY-MM-DD[T]').format('DD/MM/YYYY')}}</span>
+            </div>
+            <div class="flex flex-col md:justify-center sm:w-1/2 md:w-1/6 px-1 xl:px-2 py-2 align-middle md:text-center">
+              <strong class="block md:hidden text-sm uppercase">Created</strong>
+              <span class="">{{ $moment(item.created_at, 'YYYY-MM-DD[T]HH:mm:ss.SSS[Z]').format('DD/MM/YYYY, h:mm:ss a') }}</span>
+            </div>
+          </nuxt-link>
+        </div>
+      </div>
+      <div v-if="!cancelledJobs.length == 0" class="">
+        <AppPagination
+          :total="total"
+          :totalPages="totalPages"
+          :currentPage="currentPage"
+          :perPage="perPage"
+          @pagechanged="pagechanged"
+        />
+      </div> 
+
+      <div class="job-shield" v-if="modal"></div>
+      <transition name="slide" mode="out-in">
+        <div class="job-modal shadow-lg" v-if="modal">
+          <PracticeSessionModal @close="modal = false" :job="job" />
+        </div>
+      </transition>
+
     </div>
+  </div>
 </template>
+
 <script>
-import AppPagination from '@/components/Base/AppPagination'
-import PracticeSessionModal from '@/components/Practices/Sessions/PracticeSessionModal'
-import AppJobHeaderSort from '@/components/Base/AppJobHeaderSort'
-export default {
+  import AppPagination from '@/components/Base/AppPagination'
+  import PracticeSessionModal from '@/components/Practices/Sessions/PracticeSessionModal'
+  import AppJobHeaderSort from '@/components/Base/AppJobHeaderSort'
+  export default {
     props:['practice', 'practice-surgery'],
     components:{
       AppJobHeaderSort,
@@ -154,7 +155,7 @@ export default {
           console.log('get cancelled jobs error!!!',err)
           this.$store.commit('SET_NOTIFICATION', { ecreated_atnabled: true, status: 'danger', text: 'Something went wrong!' })
         })
-       
+      
       },
       async pagechanged(e) {
         const query = {
@@ -166,5 +167,5 @@ export default {
         await this.$store.commit('jobs/TOGGLE_LOADING', false)
       }
     }
-}
+  }
 </script>
