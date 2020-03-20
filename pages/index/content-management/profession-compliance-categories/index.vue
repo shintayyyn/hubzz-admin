a1a542b42ab8d3cfe329be9e0686d17b14710c8a
Arvi <cacheng.halcyondigital@gmail.com>
2020-03-20 20:41:10 +0800

    ...

---
+++ pages/index/content-management/profession-compliance-categories/index.vue
@@ -0,0 +1,291 @@
+<template>
+  <div class="flex-1 flex flex-col py-2 px-4 md:px-6">
+    <div class="text-xl md:text-2xl text-white">
+      Profession Compliance Categories
+    </div>
+
+    <ContentManagementTable
+      :limit="limit"
+      :items="professionComplianceCategories"
+      :getItemKey="(item) => item.id"
+      :getItemLink="(item) => `/profession-compliance-categories/${item.id}`"
+      :columnDetails="columnDetails"
+      :orderBy="orderBy"
+      :loading="loading"
+      @setOrderBy="(value) => orderBy = value"
+    />
+
+    <div class="w-full flex flex-wrap justfify-between items-center">
+      <div class="flex-1 flex flex-wrap justify-between pt-2 md:py-2 text-sm">
+        <div class="text-gray-500 w-full md:w-auto text-center md:text-left">
+          <div class="whitespace-no-wrap">
+            Showing {{ limit * activePage + 1 - limit }} to {{ activePage === pages ? count : limit * activePage }} of {{ count }} items
+          </div>
+        </div>
+      </div>
+
+      <div v-if="count > 0 && pages > 1" class="flex flex-col justify-center items-center py-2 w-full">
+        <div :class="pages > 2 ? 'flex md:hidden' : 'hidden'">
+          <div v-for="page in pages" :key="page" class="md:hidden m-1">
+            <button
+              type="button"
+              class="page-button rounded-lg py-2 px-3 md:px-4 font-bold text-xs md:text-sm focus:outline-none"
+              :disabled="loading || activePage === page"
+              :class="{
+                'active cursor-not-allowed': activePage === page,
+              }"
+              @click="activePage = page"
+            >
+              {{ page }}
+            </button>
+          </div>
+        </div>
+  
+        <div class="flex items-center">
+          <div v-if="pages > 2" class="m-1">
+            <button
+              type="button"
+              class="relative page-button rounded-lg h-full p-3 flex justify-center items-center font-bold text-sm focus:outline-none"
+              :class="{
+                'text-gray-500 cursor-not-allowed': activePage === 1,
+              }"
+              :disabled="loading || activePage === 1"
+              @click="activePage = 1"
+            >
+              <svgicon name="left-chevron" width="13" height="13" />
+            </button>
+          </div>
+  
+          <div class="m-1">
+            <button
+              type="button"
+              class="relative page-button rounded-lg h-full py-3 px-3 flex justify-center items-center font-bold text-sm focus:outline-none"
+              :class="{
+                'text-gray-500 cursor-not-allowed': activePage === 1,
+              }"
+              :disabled="loading || activePage === 1"
+              @click="activePage = activePage > 1 ? activePage - 1 : 1"
+            >
+              <svgicon name="arrow-left" width="13" height="13" />
+            </button>
+          </div>
+  
+          <div :class="pages > 2 ? 'hidden md:flex' : 'flex'">
+            <div v-for="page in pages" :key="page" class="m-1">
+              <button
+                type="button"
+                class="rounded-lg page-button py-2 px-3 md:px-4 font-bold text-xs md:text-sm focus:outline-none"
+                :disabled="loading || activePage === page"
+                :class="{
+                  'active cursor-not-allowed': activePage === page,
+                }"
+                @click="activePage = page"
+              >
+                {{ page }}
+              </button>
+            </div>
+          </div>
+  
+          <div class="next m-1">
+            <button
+              type="button"
+              class="relative page-button rounded-lg h-full p-3 font-bold text-sm focus:outline-none"
+              :class="{
+                'text-gray-500 cursor-not-allowed': activePage === pages,
+              }"
+              :disabled="loading || activePage === pages"
+              @click="activePage = activePage < pages ? activePage + 1 : pages"
+            >
+              <svgicon name="arrow-right" width="13" height="13" />
+            </button>
+          </div>
+  
+          <div v-if="pages > 2" class="m-1">
+            <button
+              type="button"
+              class="relative page-button rounded-lg h-full p-3 font-bold text-sm focus:outline-none"
+              :class="{
+                'text-gray-500 cursor-not-allowed': activePage === pages,
+              }"
+              :disabled="loading || activePage === pages"
+              @click="activePage = pages"
+            >
+              <svgicon name="right-chevron" width="13" height="13" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <nuxt-link
+      v-if="
+        $route.name !== 'index-content-management-profession-compliance-categories'
+          && $route.name !== 'index-content-management-profession-compliance-categories-index'
+      "
+      class="bg-shield z-511 fixed inset-0 opacity-50"
+      to="/profession-compliance-categories"
+    />
+  
+    <nuxt-child />
+  </div>
+</template>
+  
+<script>
+  import ContentManagementTable from '@/components/ContentManagement/ContentManagementTable'
+
+  export default {
+    components: {
+      ContentManagementTable,
+    },
+
+    data () {
+      return {
+        loading: false,
+        count: 0,
+        professionComplianceCategories: [],
+        orderBy: [],
+        limit: 5,
+        activePage: 1,
+      }
+    },
+
+    computed: {
+      offset () {
+        return this.activePage * this.limit - this.limit
+      },
+
+      columnDetails () {
+        return [
+          // {
+          //   title: '#',
+          //   key: 'index',
+          //   sort_key: null,
+          //   column: (item, index) => this.offset + index + 1,
+          //   justify: 'end',
+          //   flexGrow: 0,
+          //   flexShrink: 0,
+          // },
+          {
+            title: 'ID',
+            key: 'id',
+            sort_key: 'id',
+            column: (item) => item.id,
+            justify: 'end',
+            flexGrow: 0,
+            flexShrink: 0,
+          },
+          {
+            title: 'Name',
+            key: 'name',
+            sort_key: 'name',
+            column: (item) => item.name,
+            justify: 'start',
+            flexGrow: 1,
+            flexShrink: 0,
+          },
+          {
+            title: 'Profession Count',
+            key: 'profession_count',
+            sort_key: 'profession_count',
+            column: (item) => item.profession_count,
+            justify: 'end',
+            flexGrow: 0,
+            flexShrink: 0,
+          },
+        ]
+      },
+
+      pages () {
+        return Math.max(Math.ceil(this.count / this.limit), 1)
+      },
+    },
+
+    watch: {
+      orderBy () {
+        this.getProfessionComplianceCategories()
+      },
+
+      limit () {
+        this.page = 1
+        this.getProfessionComplianceCategories()
+      },
+
+      activePage () {
+        this.getProfessionComplianceCategories()
+      },
+    },
+
+    mounted () {
+      this.getProfessionComplianceCategories()
+    },
+
+    methods: {
+      getProfessionComplianceCategories () {
+        this.loading = true
+        this.professionComplianceCategories = []
+        Promise.all([
+          this.$axios.get('/api/v1/admin/profession-compliance-categories/count').then((responses) => {
+            return responses.data.data.count
+          }),
+          this.$axios.get('/api/v1/admin/profession-compliance-categories', {
+            params: {
+              order_by: this.orderBy,
+              limit: this.limit,
+              offset: this.offset,
+            },
+          }).then((responses) => {
+            return responses.data.data.profession_compliance_categories
+          }),
+          new Promise((resolve) => setTimeout(resolve, 500))
+        ]).then((results) => {
+          const [
+            count,
+            professionComplianceCategories,
+          ] = results
+
+          this.count = count
+          this.professionComplianceCategories = professionComplianceCategories
+        }).catch((err) => {
+          console.log('err', err)
+          this.$nuxt.error(err)
+        }).finally(() => {
+          this.loading = false
+        })
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  .page-button {
+      background: linear-gradient(to top, #f2d024, #efde86);
+      color: #000;
+  }
+  .active {
+      background: linear-gradient(to top, #dbb013, #ecc94b);
+      color: #000;
+      box-shadow: 0 3px 5px #333;
+  }
+  button.active:disabled {
+      color: #f2d024;
+      box-shadow: inset 0px 0px 0px 2px #dbb013;
+      cursor: default;
+  }
+  
+  button:active {
+      transform: translate(2px, 2px);
+  }
+  
+  button:active :not(button:disabled) {
+      transform: translate(2px, 2px);
+      box-shadow: 0 0 0 transparent;
+  }
+  
+  button:disabled,
+  button:disabled svg {
+      background: #696c71;
+      color: #999999;
+      fill: #999999;
+      box-shadow: 0 0 0 transparent;
+  }
+</style>
--- pages/index/reports/qwe.vue
+++ pages/index/reports/qwe.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="report-modal p-4 md:p-8 shadow-lg">
-    <ReportModal/>
+    <ReportModal />
   </div>
 </template>
 <script>
@@ -9,7 +9,7 @@ export default {
   components:{
     ReportModal
   },
-  data(){
+  data (){
     return{
 
     }
@@ -17,33 +17,3 @@ export default {
   
 }
 </script>
-<style>
-.report-shield {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #333;
-  opacity: 0.5;
-  z-index: 511;
-}
-.report-modal {
-  position: fixed;
-  top: 0;
-  right: 0;
-  margin-right: 0%;
-  width: 100%;
-  height: 100%;
-  overflow: auto;
-  border-left: solid 2px #FFC72C;
-  transition: all 0.3s ease-in-out;
-  background-color:#505561;
-  z-index: 512;
-}
-@media screen and (min-width: 1200px) {
-  .report-modal {
-    width: 80%;
-  }
-}
-</style>
