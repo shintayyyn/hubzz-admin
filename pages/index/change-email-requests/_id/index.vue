ac4c55ca61d963a3fe6b1840f903fa91a91e925a
Arvi <cacheng.halcyondigital@gmail.com>
2020-06-17 01:59:36 +0800

    ...

---
+++ pages/index/change-email-requests/_id/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <nuxt-child
+    :changeEmailRequests="changeEmailRequests"
+    :changeEmailRequestStatusColorClass="changeEmailRequestStatusColorClass"
+    @changeEmailRequests="(_changeEmailRequests) => $emit('changeEmailRequests', _changeEmailRequests)"
+  />
+</template>
+
+<script>
+  export default {
+    props: {
+      changeEmailRequests: {
+        type: Array,
+        required: true,
+      },
+
+      changeEmailRequestStatusColorClass: {
+        type: Object,
+        required: true,
+      },
+    },
+  }
+</script>
