78cfedba485ddebc8e04f7f536dad4cf90f074cc
Arvi <cacheng.halcyondigital@gmail.com>
2020-07-17 18:13:39 +0800

    ...

--- components/Practices/PracticeTabs.vue
+++ components/Practices/PracticeTabs.vue
@@ -4,73 +4,96 @@
       :to="getRoute()"
       class="px-4 py-3 mr-2 text-sm font-bold cursor-pointer rounded-lg whitespace-no-wrap transition-hover"
       :class="$route.path == `/practices/${practice.id}` ? 'bg-sunglow hover:bg-sunglow-dark' : 'hover:bg-waterloo text-white'"
-    >Practice</nuxt-link>
+    >
+      Practice
+    </nuxt-link>
     <nuxt-link
-      :to="getRoute('practice-surgeries')"
       v-if="practice.type == 'Hub' && (practice.status !== 'Inactive' && practice.status !== 'Bogus' && practice.status !== 'Deactivated')"
+      :to="getRoute('practice-surgeries')"
       class="px-4 py-3 mr-2 text-sm font-bold cursor-pointer rounded-lg whitespace-no-wrap transition-hover"
       :class="$route.path.includes(`/practices/${practice.id}/practice-surgeries`)? 'bg-sunglow hover:bg-sunglow-dark' : 'hover:bg-waterloo text-white'"
-    >Surgery Management</nuxt-link>
+    >
+      Surgery Management
+    </nuxt-link>
     <nuxt-link
-      :to="getRoute('practice-hub')"
       v-if="practice.type == 'Spoke' && (practice.status !== 'Inactive' && practice.status !== 'Bogus' && practice.status !== 'Deactivated')"
+      :to="getRoute('practice-hub')"
       class="px-4 py-3 mr-2 text-sm font-bold cursor-pointer rounded-lg whitespace-no-wrap transition-hover"
       :class="$route.path == `/practices/${practice.id}/practice-hub`? 'bg-sunglow hover:bg-sunglow-dark' : 'hover:bg-waterloo text-white'"
-    >Hub</nuxt-link>
+    >
+      Hub
+    </nuxt-link>
     <nuxt-link
-      :to="getRoute('practice-invitations')"
       v-if="(practice.status !== 'Inactive' && practice.status !== 'Bogus' && practice.status !== 'Deactivated')"
+      :to="getRoute('practice-invitations')"
       class="px-4 py-3 mr-2 text-sm font-bold cursor-pointer rounded-lg whitespace-no-wrap transition-hover"
       :class="$route.name.includes('index-practices-id-index-practice-invitations-index') ? 'bg-sunglow hover:bg-sunglow-dark' : 'hover:bg-waterloo text-white'"
-    >{{`Invitation${practice.type === 'Hub' ? 's' : ''}`}}</nuxt-link>
+    >
+      {{ `Invitation${practice.type === 'Hub' ? 's' : ''}` }}
+    </nuxt-link>
     <nuxt-link
       v-if="practice.status !== 'Inactive' && practice.status !== 'Bogus'"
       :to="getRoute('practice-sessions')"
       class="px-4 py-3 mr-2 text-sm font-bold cursor-pointer rounded-lg whitespace-no-wrap transition-hover"
       :class="$route.path.includes(`/practices/${practice.id}/practice-sessions`) ? 'bg-sunglow hover:bg-sunglow-dark' : 'hover:bg-waterloo text-white'"
-    >Sessions</nuxt-link>
+    >
+      Sessions
+    </nuxt-link>
     <nuxt-link
-      :to="getRoute('practice-users')"
       v-if="practice.status !== 'Deactivated'"
+      :to="getRoute('practice-users')"
       class="px-4 py-3 mr-2 text-sm font-bold cursor-pointer rounded-lg whitespace-no-wrap transition-hover"
       :class="$route.path.includes(`/practices/${practice.id}/practice-users`) ? 'bg-sunglow hover:bg-sunglow-dark' : 'hover:bg-waterloo text-white'"
-    >Users</nuxt-link>
+    >
+      Users
+    </nuxt-link>
     <nuxt-link
-      :to="getRoute('practice-documents')"
       v-if="practice.status !== 'Bogus' && practice.status !== 'Deactivated'"
+      :to="getRoute('practice-documents')"
       class="px-4 py-3 mr-2 text-sm font-bold cursor-pointer rounded-lg whitespace-no-wrap transition-hover"
       :class="$route.path == `/practices/${practice.id}/practice-documents` ? 'bg-sunglow hover:bg-sunglow-dark' : 'hover:bg-waterloo text-white'"
-    >Documents</nuxt-link>
+    >
+      Documents
+    </nuxt-link>
     <nuxt-link
-      :to="getRoute('practice-rates')"
       v-if="practice.status !== 'Bogus' && practice.status !== 'Deactivated'"
+      :to="getRoute('practice-rates')"
       class="px-4 py-3 mr-2 text-sm font-bold cursor-pointer rounded-lg whitespace-no-wrap transition-hover"
       :class="$route.path == `/practices/${practice.id}/practice-rates` ? 'bg-sunglow hover:bg-sunglow-dark' : 'hover:bg-waterloo text-white'"
-    >Rates</nuxt-link>
+    >
+      Rates
+    </nuxt-link>
   </div>
 </template>
+
 <script>
 export default {
-  props: ["practice"],
+  props: {
+    practice: {
+      type: Object,
+      default: () => null,
+    }
+  },
+
   computed: {
-    getRoute() {
+    getRoute () {
       return tab => {
         if (!tab) {
-          tab = "";
+          tab = ""
         }
         const query = {
           ...this.$route.query
-        };
-        delete query.order_by;
-        delete query.status;
+        }
+        delete query.order_by
+        delete query.status
         return {
           path: tab
             ? `/practices/${this.practice.id}/${tab}`
             : `/practices/${this.practice.id}`,
           query
-        };
-      };
+        }
+      }
     }
   }
-};
+}
 </script>
