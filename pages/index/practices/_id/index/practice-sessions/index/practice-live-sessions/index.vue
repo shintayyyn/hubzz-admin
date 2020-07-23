39e979ae2d0576e4333c7cc4e0b542702cc37b97
Arvi <cacheng.halcyondigital@gmail.com>
2020-07-23 15:11:02 +0800

    ...

--- pages/index/practices/_id/index/practice-sessions/index/practice-live-sessions/index.vue
+++ pages/index/practices/_id/index/practice-sessions/index/practice-live-sessions/index.vue
@@ -1,34 +1,22 @@
 <template>
   <div>
-    <PracticeLiveSessions :practice='practice'/>
+    <PracticeLiveSessions :practice="practice" />
   </div>
 </template>
+
 <script>
 import PracticeLiveSessions from '@/components/Practices/Sessions/PracticeLiveSessions'
+
 export default {
   components:{
     PracticeLiveSessions
   },
-  computed:{
-    practice(){
-      return this.$store.state.practices.practice
-    }
-  },
-  async asyncData({ app, store, route}){
-    try{
-      let response = await app.$axios.$get(`/api/v1/admin/practices/${route.params.id}`)
-      const practice = response.data.practice
-      await store.commit('practices/SET_SPECIFIC_PRACTICE',practice)
-      return{
 
-      }
-    }catch(err){
-      store.commit('SET_NOTIFICATION',{ enabled: true, status:'danger', text:'Something went wrong!'})
-      console.log('get practice error!!!!',err)
-    }
-  }
+  props: {
+    practice: {
+      type: Object,
+      default: () => null,
+    },
+  },
 }
 </script>
-<style>
-
-</style>
