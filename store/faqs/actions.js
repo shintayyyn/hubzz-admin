0fc1d6ee1fc2de39258878949e84642403074fec
Zarley Vedad <zpveded.halcyondigital@gmail.com>
2020-11-16 17:43:49 +0800

    fixes

--- store/faqs/actions.js
+++ store/faqs/actions.js
@@ -1,6 +1,6 @@
 import * as faqApi from '@/api/faqs'
 export default{
-    async initializeFaqTransactionListener({state, commit}){
+    async initializeFaqTransactionListener ({commit}){
         this.$socket.on("createdFaq",faqModel => {
             commit('ADD_FAQ', faqModel)
         })
@@ -13,12 +13,12 @@ export default{
             commit('DELETE_FAQ', existingFaq)
         })
     },
-    async fetchLocumFaqs({ commit },payload){
+    async fetchLocumFaqs ({ commit },payload){
         commit('TOGGLE_LOADING',true)        
         const locum_faqs = await faqApi.fetchLocumFaqs(this.$axios, payload)
         return commit('SET_LOCUM_FAQS', locum_faqs.data.faqs)
     },
-    async fetchPracticeFaqs({ commit }, payload){
+    async fetchPracticeFaqs ({ commit }, payload){
         commit('TOGGLE_LOADING',true)
         const practice_faqs = await faqApi.fetchPracticeFaqs(this.$axios, payload)
         commit('TOGGLE_LOADING',false)
