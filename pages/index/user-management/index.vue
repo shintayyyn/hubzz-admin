def64af21e179ad39b9acfaa140b3cc01efb3927
DevJarbs <jarveeviosbelarmino@gmail.com>
2026-03-17 08:52:14 +0800

    testing

--- pages/index/user-management/index.vue
+++ pages/index/user-management/index.vue
@@ -235,10 +235,13 @@ export default {
       if (this.adminAccountId) {
         await this.$axios
           .$delete(`/api/v1/admin/admin-users/${this.adminAccountId}`)
-          .then(res => {
-            console.log(res)
-            this.$store.getters['adminusers/getAdminUsers']
-            this.$emit('close')
+          .then(async res => {
+            await this.$store.dispatch('adminusers/fetchAdminUsers', {
+              limit: 10,
+              offset: this.getQuery(),
+              search: this.search
+            })
+
             this.$store.commit('SET_NOTIFICATION', {
               enabled: true,
               status: 'success',
